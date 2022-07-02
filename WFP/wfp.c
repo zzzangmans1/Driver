564058a727c0d9195588ae172cd0e281e36689f1
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2022-07-02 19:58:09 +0900

    Update wfp.c

--- WFP/wfp.c
+++ WFP/wfp.c
@@ -5,7 +5,7 @@
 #include <guiddef.h>
 #include <fwpmu.h>
 
-
+#pragma warning(disable: 4100)
 
 DEFINE_GUID(WFP_SAMPLE_ESTABLEISHED_CALLOUT_V4_GUID, 0xd969fc67, 0x6fb2, 0x4504, 0x91, 0xce, 0xa9, 0x7c, 0x3c, 0x32, 0xad, 0x36);
 DEFINE_GUID(WFP_SAMPLE_SUB_LAYER_GUID, 0xed6a516a, 0x36d1, 0x4881, 0xbc, 0xf0, 0xac, 0xeb, 0x4c, 0x4, 0xc2, 0x1c);
@@ -26,7 +26,7 @@ VOID UnInitWfp()
 		if (filterid != 0)
 		{
 			FwpmFilterDeleteById(EngineHandle, filterid);
-			FwpmCalloutDeleteById(EngineHandle, &WFP_SAMPLE_SUB_LAYER_GUID);
+			FwpmSubLayerDeleteByKey(EngineHandle, &WFP_SAMPLE_SUB_LAYER_GUID);
 		}
 
 		if (AddCalloutId != 0)
@@ -50,7 +50,7 @@ VOID Unload(PDRIVER_OBJECT DriverObject)
 	KdPrint(("Unload \r\n"));
 }
 
-NTSTATUS NotifyCallback(FWPS_CALLOUT_NOTIFY_TYPE type, GUID* filterkey, FWPS_FILTER* filter)
+NTSTATUS NotifyCallback(FWPS_CALLOUT_NOTIFY_TYPE type, const GUID* filterkey, const FWPS_FILTER* filter)
 {
 	return STATUS_SUCCESS;
 }
@@ -61,8 +61,43 @@ VOID FlowDelteCallback(UINT16 layerid, UINT32 calloutid, UINT64 flowcontext)
 }
 
 // 가장중요한 필터
-VOID FilterCallback(FWPS_INCOMING_VALUE* Values, FWPS_INCOMING_METADATA_VALUES0 MetaData, PVOID* layerdata, PVOID context, FWPS_FILTER* filter, UINT64 flowcontext, FWPS_CLASSIFY_OUT* classify)
+VOID FilterCallback(const FWPS_INCOMING_VALUES0* Values, const FWPS_INCOMING_METADATA_VALUES0* MetaData, PVOID layerdata, const void* context, const FWPS_FILTER* filter, UINT64 flowcontext, FWPS_CLASSIFY_OUT0* classifyout)
 {
+	FWPS_STREAM_CALLOUT_IO_PACKET* packet;
+	FWPS_STREAM_DATA* streamdata;
+	UCHAR string[201] = { 0 };
+	ULONG length = 0;
+	SIZE_T bytes;
+	KdPrint(("data is here \r\n"));
+
+	packet = (FWPS_STREAM_CALLOUT_IO_PACKET*)layerdata;
+	streamdata = packet->streamData;
+
+	RtlZeroMemory(classifyout, sizeof(FWPS_CLASSIFY_OUT));
+	/*
+	if ((streamdata->flags & FWPS_STREAM_FLAG_SEND) || (streamdata->flags & FWPS_STREAM_FLAG_SEND_EXPEDITED) || (streamdata->flags & FWPS_STREAM_FLAG_RECEIVE_EXPEDITED))
+	{
+		goto end;
+	}
+	*/
+	if ((streamdata->flags & FWPS_STREAM_FLAG_RECEIVE))
+	{
+		// receive
+		length = streamdata->dataLength <= 200 ? (ULONG)streamdata->dataLength : 200;
+
+		FwpsCopyStreamDataToBuffer(streamdata, string, length, &bytes);
+
+		KdPrint(("data is %s\r\n", string));
+	}
+
+//end:
+	packet->streamAction = FWPS_STREAM_ACTION_NONE;
+	classifyout->actionType = FWP_ACTION_PERMIT;
+
+	if (filter->flags & FWPS_FILTER_FLAG_CLEAR_ACTION_RIGHT)
+	{
+		classifyout->rights &= ~FWPS_RIGHT_ACTION_WRITE;
+	}
 
 }
 
@@ -83,7 +118,7 @@ NTSTATUS WfpRegisterCallout()
 	Callout.notifyFn = NotifyCallback;
 	Callout.flowDeleteFn = FlowDelteCallback;
 
-	FwpsCalloutRegister(DeviceObject, &Callout, &RegCalloutId);
+	return FwpsCalloutRegister(DeviceObject, &Callout, &RegCalloutId);
 }
 
 // 필요한 기능 바로 호출
@@ -94,7 +129,7 @@ NTSTATUS WfpAddCallout()
 	callout.flags = 0;
 	callout.displayData.name = L"EstablishedCalloutName";
 	callout.displayData.description = L"EstablishedCalloutName";
-	callout.calloutId = WFP_SAMPLE_ESTABLEISHED_CALLOUT_V4_GUID;
+	callout.calloutKey= WFP_SAMPLE_ESTABLEISHED_CALLOUT_V4_GUID;
 	callout.applicableLayer = FWPM_LAYER_STREAM_V4;				// 레이어 스트림
 
 	return FwpmCalloutAdd(EngineHandle, &callout, NULL, &AddCalloutId);
@@ -133,7 +168,7 @@ NTSTATUS WfpAddFilter()
 	condition[0].conditionValue.type = FWP_UINT16;
 	condition[0].conditionValue.uint16 = 65000;
 
-	FwpmFilterAdd(EngineHandle, &filter, NULL, &filterid);
+	return FwpmFilterAdd(EngineHandle, &filter, NULL, &filterid);
 }
 
 // 초기화 작업
@@ -159,11 +194,11 @@ NTSTATUS InitializeWfp()
 		goto end;
 	}
 
-	if (!NT_SUCCESS(WfpAddfilter()))
+	if (!NT_SUCCESS(WfpAddFilter()))
 	{
 		goto end;
 	}
-	
+
 	return STATUS_SUCCESS;
 end:
 	UnInitWfp();
@@ -193,6 +228,6 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 		IoDeleteDevice(DeviceObject); // 실패하면 무조건 장치 삭제해줘야한다.
 		return status;
 	}
-
+	KdPrint(("Driver Load"));
 	return status;
 }
